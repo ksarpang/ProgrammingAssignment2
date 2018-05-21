98f595833e0613a8a37d8dafdecbfac962f1f02c
ksarpang <kishan.sarpangala@hotmail.com>
2018-05-21 02:48:45 -0400

    Test the functions
    Test the functions with the examples provided.

---
+++ Test_examples_for_Inverse_of_matrix.R
@@ -0,0 +1,17 @@
+#Test_both_the functions_using the below examples. 
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+my_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+
